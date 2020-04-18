1dc42ef2ac27f9925f890d11bedeff413c9bed03
Davidtrejo590 <61710801+Davidtrejo590@users.noreply.github.com>
2020-04-18 15:50:28 -0500

    Adaptación de Código

--- proyectoCG/proyectoCG/lego.cpp
+++ proyectoCG/proyectoCG/lego.cpp
@@ -1,28 +1,24 @@
 /*--------------------- PROYECTO: ESCENARIO LEGO ------------------------------------*/
-
 /*--------------------Integrantes: ----------------------------------------*/
 /*------------------ Amezaga Campos Salvador ---------------------------------*/
 /*------------------ Colohua Carvajal Daniela ---------------------------------*/
+/*------------------ Luis David Torres Trejo ---------------------------------*/
 
-#define STB_IMAGE_IMPLEMENTATION
-
+//#define STB_IMAGE_IMPLEMENTATION
 #include <glew.h>
 #include <glfw3.h>
-
 #include <stb_image.h>
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
-#include <shader_m.h>
-
-#include <iostream>
-
 #include "camera.h"
+#include "Model.h"
+#include "Texture.h"
+
+// Other Libs
+#include "SOIL2/SOIL2.h"
 
 void resize(GLFWwindow* window, int width, int height);
-void my_input(GLFWwindow *window);
+//void my_input(GLFWwindow *window);
+void my_input(GLFWwindow *window, int key, int scancode, int action, int mode);
 void mouse_callback(GLFWwindow *window, double xpos, double ypos);
 void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
 
@@ -32,38 +28,56 @@ int SCR_WIDTH = 3800;
 int SCR_HEIGHT = 7600;
 
 GLFWmonitor *monitors;
-GLuint VBO, VAO, EBO;
+GLuint skyboxVBO, skyboxVAO;
 
 //Camera
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 double	lastX = 0.0f,
-lastY = 0.0f;
+		lastY = 0.0f;
 bool firstMouse = true;
 
 //Timing
 double	deltaTime = 0.0f,
-lastFrame = 0.0f;
+		lastFrame = 0.0f;
+
+//Lighting
+glm::vec3 lightPosition(0.0f, 4.0f, 3.0f);
+glm::vec3 lightDirection(0.0f, -1.0f, -1.0f);
 
 void myData(void);
-void display(Shader);
+void display(Shader, Model, Model);
 void getResolution(void);
 void animate(void);
 void LoadTextures(void);
 unsigned int generateTextures(char*, bool);
 
-//For Keyboard
-float	movX = 0.0f,
-		movY = 0.0f,
-		movZ = -5.0f,
-		rotX = 0.0f,
-		rotY = 0.0f,
-		rotZ = 0.0f,
-		i = 0;
-
 //Texture
-unsigned int t_piezaL,
-			 t_piezaGris,
-			 t_pBlanca;
+unsigned int	t_smile,
+				t_toalla,
+				t_unam,
+				t_white,
+				t_panda,
+				t_cubo,
+				t_caja,
+				t_caja_brillo;
+
+//For model
+bool animacion = false;
+
+/* VARIABLES PARA EL MOVIMIENTO DEL AUTO */
+
+float	movAuto_z = 0.0f,
+		movAuto_y = -1.75f;
+
+/* VARIABLES DE CONTROL */
+
+bool	avanza = false,			// MOVIMIENTO HORIZONTAL
+		sube = true,			// MOVIMIENTO VERTICAL
+		EA = true,				// ENABLE HORIZONTAL
+		ES = false,				// ENABLE VERTICAL
+		AV = false;				// ENABLE FINAL (PARA EL ULTIMO DESPLAZAMIENTO)
+	
+
 
 unsigned int generateTextures(const char* filename, bool alfa)
 {
@@ -79,7 +93,7 @@ unsigned int generateTextures(const char* filename, bool alfa)
 	// load image, create texture and generate mipmaps
 	int width, height, nrChannels;
 	stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-
+	
 	unsigned char *data = stbi_load(filename, &width, &height, &nrChannels, 0);
 	if (data)
 	{
@@ -99,7 +113,6 @@ unsigned int generateTextures(const char* filename, bool alfa)
 	stbi_image_free(data);
 }
 
-
 void getResolution()
 {
 	const GLFWvidmode * mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
@@ -114,14 +127,60 @@ void getResolution()
 
 void LoadTextures()
 {
-
-	t_piezaL = generateTextures("Textures/figura.jpg", 0);
-	t_piezaGris = generateTextures("Textures/piezaGris.jpg", 0);
-	t_pBlanca = generateTextures("Textures/pBlanca.jpg", 0);
+	
 }
 
 void myData()
-{
+{	
+	/* VERTICES PARA EL SKY BOX */
+
+	GLfloat skyboxVertices[] = {
+		// Positions
+		-1.0f,  1.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f,  1.0f, -1.0f,
+		-1.0f,  1.0f, -1.0f,
+
+		-1.0f, -1.0f,  1.0f,
+		-1.0f, -1.0f, -1.0f,
+		-1.0f,  1.0f, -1.0f,
+		-1.0f,  1.0f, -1.0f,
+		-1.0f,  1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f,
+
+		1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+
+		-1.0f, -1.0f,  1.0f,
+		-1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f, -1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f,
+
+		-1.0f,  1.0f, -1.0f,
+		1.0f,  1.0f, -1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		-1.0f,  1.0f,  1.0f,
+		-1.0f,  1.0f, -1.0f,
+
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f,  1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f,  1.0f,
+		1.0f, -1.0f,  1.0f
+	};
+
+	/* VERTICES DE UN CUBO */
+
 	float vertices[] = {
 
 		-0.5f, -0.5f, 0.5f,		0.0f,0.0f,0.0f,	//V0 - Frontal
@@ -154,45 +213,60 @@ void myData()
 		0.5f, -0.5f, -0.5f,		1.0f,1.0f,0.0f,	//V2
 		0.5f, -0.5f, 0.5f,		0.0f,1.0f,0.0f,
 	};
-	unsigned int indices[] = {
-		0, 1, 3, // first triangle
-		1, 2, 3  // second triangle
-	};
 
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	glGenVertexArrays(1, &skyboxVAO);
+	glGenBuffers(1, &skyboxVBO);
 
-	glBindVertexArray(VAO);
+	glBindVertexArray(skyboxVAO);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	// SkyBox
 	glEnableVertexAttribArray(0);
-	// texture coord attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0);
 
 }
 
+/**/
 void animate(void)
 {
-
 }
 
-void display(Shader shader)
+void display(Shader shader, Shader skyboxShader, GLuint skybox,Model piso, Model PisoB)
 {
-	//Enable Shader
 	shader.use();
 
+	//Setup Advanced Lights
+
+	/* FUENTE DE LUZ DIRECCIONAL */
+
+	shader.setVec3("viewPos", camera.Position);
+	shader.setVec3("dirLight.direction", lightDirection);
+	shader.setVec3("dirLight.ambient", glm::vec3(0.0f, 0.0f, 0.0f));
+	shader.setVec3("dirLight.diffuse", glm::vec3(1.0f, 1.0f, 1.0f));
+	shader.setVec3("dirLight.specular", glm::vec3(1.0f, 1.0f, 1.0f));
+
+	shader.setVec3("pointLight[0].position", lightPosition);
+	shader.setVec3("pointLight[0].ambient", glm::vec3(0.0f, 0.0f, 0.0f));
+	shader.setVec3("pointLight[0].diffuse", glm::vec3(1.0f, 1.0f, 1.0f));
+	shader.setVec3("pointLight[0].specular", glm::vec3(1.0f, 1.0f, 1.0f));
+	shader.setFloat("pointLight[0].constant", 1.0f);						/* DISTANCIA DE LOS RAYOS LUMINOSOS */
+	shader.setFloat("pointLight[0].linear", 0.009f);						/*  */
+	shader.setFloat("pointLight[0].quadratic", 0.00032f);					/* PARA MAYOR ILUMINACION (POTENCIA)*/
+
+	shader.setVec3("pointLight[1].position", glm::vec3(0.0, 0.0f, 0.0f));
+	shader.setVec3("pointLight[1].ambient", glm::vec3(0.0f, 0.0f, 0.0f));
+	shader.setVec3("pointLight[1].diffuse", glm::vec3(0.0f, 0.0f, 0.0f));
+	shader.setVec3("pointLight[1].specular", glm::vec3(0.0f, 0.0f, 0.0f));
+	shader.setFloat("pointLight[1].constant", 1.0f);
+	shader.setFloat("pointLight[1].linear", 0.009f);
+	shader.setFloat("pointLight[1].quadratic", 0.032f);
+	
+	shader.setFloat("material_shininess", 32.0f);
+
 	// create transformations and Projection
-	glm::mat4 temp = glm::mat4(1.0f);
-	glm::mat4 temp2 = glm::mat4(1.0f);
+	glm::mat4 tmp = glm::mat4(1.0f);
 	glm::mat4 model = glm::mat4(1.0f);		// initialize Matrix, Use this matrix for individual models
 	glm::mat4 view = glm::mat4(1.0f);		//Use this matrix for ALL models
 	glm::mat4 projection = glm::mat4(1.0f);	//This matrix is for Projection
@@ -202,92 +276,84 @@ void display(Shader shader)
 	view = camera.GetViewMatrix();
 
 	// pass them to the shaders
-	shader.setVec3("viewPos", camera.Position);
 	shader.setMat4("model", model);
 	shader.setMat4("view", view);
 	// note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
 	shader.setMat4("projection", projection);
 
+	/* DIBUJAMOS EL PISO */
 
-	glBindVertexArray(VAO);
-	//Colocar código aquí
+	/* EL MODELO DEL PISO ES UN CUBO (MEDIDAS DEL OBJ (1,1,0.025)[m])*/
 
-	
-	for (i = 0; i < 5; i++) {
-		for (float j = 0; j < 20; j++) {
-			model = glm::translate(glm::mat4(1.0f), glm::vec3(-i, 0.0f, j));
-			model = glm::scale(model, glm::vec3(1.0f, 0.1f, 1.0f));
-			shader.setMat4("model", model);
-			shader.setVec3("aColor", 1.0f, 1.0f, 1.0f);
-			glBindTexture(GL_TEXTURE_2D, t_piezaGris);
-			glDrawArrays(GL_QUADS, 0, 24);
-		}
-	}
+	/* PIEZA GRIS */
 
-	for (i = 0; i < 20; i++) {
-		model = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 0.0f, i));
-		model = glm::scale(model, glm::vec3(1.0f, 0.1f, 1.0f));
-		shader.setMat4("model", model);
-		shader.setVec3("aColor", 1.0f, 1.0f, 1.0f);
-		glBindTexture(GL_TEXTURE_2D, t_pBlanca);
-		glDrawArrays(GL_QUADS, 0, 24);
-	}
+	model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f,-1.5f,0.0f));
+	model = glm::scale(model, glm::vec3(5.0f, 1.0f, 15.0f));
+	shader.setMat4("model", model);
+	piso.Draw(shader);
 
-	for (i = 1; i < 6; i++) {
-		for (float j = 0; j < 20; j++) {
-			model = glm::translate(glm::mat4(1.0f), glm::vec3(i, 0.0f, j));
-			model = glm::scale(model, glm::vec3(1.0f, 0.1f, 1.0f));
-			shader.setMat4("model", model);
-			shader.setVec3("aColor", 1.0f, 1.0f, 1.0f);
-			glBindTexture(GL_TEXTURE_2D, t_piezaGris);
-			glDrawArrays(GL_QUADS, 0, 24);
-		}
-	}
-	/*
-	for (i = 0; i < 10; i++) {
-		for (float j = 0; j < 10; j++) {
-			model = glm::translate(glm::mat4(1.0f), glm::vec3(j, 0.0f, i));
-			model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
-			shader.setMat4("model", model);
-			shader.setVec3("aColor", 1.0f, 1.0f, 1.0f);
-			glBindTexture(GL_TEXTURE_2D, t_piezaL);
-			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		}
-	}
-	*/
+	/* PIEZA BLANCA */
+
+	model = glm::translate(glm::mat4(1.0f), glm::vec3(5.0f, -1.5f, 0.0f));
+	model = glm::scale(model, glm::vec3(5.0f, 1.0f, 15.0f));
+	shader.setMat4("model", model);
+	PisoB.Draw(shader);
+
+	/* SEGUNDA PIEZA GRIS */
+
+	model = glm::translate(glm::mat4(1.0f), glm::vec3(10.0f,-1.5f,0.0f));
+	model = glm::scale(model, glm::vec3(5.0f, 1.0f, 15.0f));
+	shader.setMat4("model", model);
+	piso.Draw(shader);
+
+	// Draw skybox as last
+	glDepthFunc(GL_LEQUAL);  // Change depth function so depth test passes when values are equal to depth buffer's content
+	skyboxShader.use();
+	view = glm::mat4(glm::mat3(camera.GetViewMatrix()));	// Remove any translation component of the view matrix
+
+	skyboxShader.setMat4("view", view);
+	skyboxShader.setMat4("projection", projection);
+
+	// skybox cube
+	glBindVertexArray(skyboxVAO);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, skybox);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
 	glBindVertexArray(0);
+	glDepthFunc(GL_LESS); // Set depth function back to default
 }
 
 int main()
 {
-	// glfw: initialize and configure
-	// ------------------------------
-	glfwInit();
-	/*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    /*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
 
 #ifdef __APPLE__
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
 #endif
 
-	// glfw window creation
-	// --------------------
+    // glfw window creation
+    // --------------------
 	monitors = glfwGetPrimaryMonitor();
 	getResolution();
 
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LEGO", NULL, NULL);
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Lego", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
 	glfwSetWindowPos(window, 0, 30);
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, resize);
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, resize);
 	glfwSetCursorPosCallback(window, mouse_callback);
 	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetKeyCallback(window, my_input);
 
 	//To Enable capture of our mouse
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
@@ -299,51 +365,78 @@ int main()
 	//Datos a utilizar
 	LoadTextures();
 	myData();
-
 	glEnable(GL_DEPTH_TEST);
+	
+	//For Models
+	Shader modelShader("Shaders/shader_Lights.vs", "Shaders/shader_Lights.fs");
+	//For Primitives
+	//Shader primitivasShader("shaders/shader_texture_color.vs", "shaders/shader_texture_color.fs");
+	Shader SkyBoxshader("Shaders/SkyBox.vs", "Shaders/SkyBox.frag");
 
-	Shader projectionShader("shaders/shader_texture_color.vs", "shaders/shader_texture_color.fs");
+	/* CARGA DE MODELOS */
 
+	// Load model
+	//Model ourModel = ((char *)"Models/Lambo/carroseria.obj");
+	//Model llantasModel = ((char *)"Models/Lambo/Wheel.obj");
+	Model pisoModel = ((char *)"Models/piso/piso.obj");
+	Model pisoBlanco = ((char *)"Models/pisoB/piso.obj");
+
+
+	/* TEXTURAS DEL SKY BOX*/
+
+	// Load textures
+	vector<const GLchar*> faces;
+	faces.push_back("SkyBox/right.tga");
+	faces.push_back("SkyBox/left.tga");
+	faces.push_back("SkyBox/top.tga");
+	faces.push_back("SkyBox/bottom.tga");
+	faces.push_back("SkyBox/back.tga");
+	faces.push_back("SkyBox/front.tga");
+
+	GLuint cubemapTexture = TextureLoading::LoadCubemap(faces);
+    
+	glm::mat4 projection = glm::mat4(1.0f);	//This matrix is for Projection
+	projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 	// render loop
-	// While the windows is not closed
-	while (!glfwWindowShouldClose(window))
-	{
+    // While the windows is not closed
+    while (!glfwWindowShouldClose(window))
+    {
 		// per-frame time logic
 		// --------------------
 		double currentFrame = glfwGetTime();
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
-		// input
-		// -----
-		my_input(window);
+        // input
+        // -----
+        //my_input(window);
 		animate();
 
-		// render
-		// Backgound color
-		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // render
+        // Backgound color
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		//Mi función de dibujo
-		display(projectionShader);
+		//display(modelShader, ourModel, llantasModel);
+		display(modelShader, SkyBoxshader, cubemapTexture,pisoModel,pisoBlanco);
 
-		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-		// -------------------------------------------------------------------------------
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
-	// glfw: terminate, clearing all previously allocated GLFW resources.
-	// ------------------------------------------------------------------
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+	glDeleteVertexArrays(1, &skyboxVAO);
+	glDeleteBuffers(1, &skyboxVBO);
 
-	glfwTerminate();
-	return 0;
+    glfwTerminate();
+    return 0;
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void my_input(GLFWwindow *window)
+void my_input(GLFWwindow *window, int key, int scancode, int action, int mode)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
@@ -356,20 +449,24 @@ void my_input(GLFWwindow *window)
 		camera.ProcessKeyboard(LEFT, (float)deltaTime);
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 		camera.ProcessKeyboard(RIGHT, (float)deltaTime);
-
-	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-		rotX += 0.18f;
 	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-		rotX -= 0.18f;
+		lightPosition.z -=0.5f;
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+		lightPosition.z += 0.5f;
 
+	/* MOVIMIENTO DEL AUTO */
+
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+		animacion = true;
 }
+
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
 void resize(GLFWwindow* window, int width, int height)
 {
-	// Set the Viewport to the size of the created window
-	glViewport(0, 0, width, height);
+    // Set the Viewport to the size of the created window
+    glViewport(0, 0, width, height);
 }
 
 // glfw: whenever the mouse moves, this callback is called
